d55bf0111959b74cc616ba7eb2fce7ee65ce6afc
aitorllamas <aitorllj93@gmail.com>
2026-08-20 23:13:39 +0200

    update registry dependencies

--- registry/ui/pxl/button.tsx
+++ registry/ui/pxl/button.tsx
@@ -1,6 +1,5 @@
 import { Button as ButtonPrimitive } from "@base-ui/react/button";
 import { cva, type VariantProps } from "class-variance-authority";
-import type { ElementType } from "react";
 
 import { cn } from "@/lib/utils";
 
