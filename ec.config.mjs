3b463e2fa3ab8c59bf082474b32cd5c404ca433e
aitorllamas <aitorllj93@gmail.com>
2026-09-01 14:05:36 +0200

    update theme

--- ec.config.mjs
+++ ec.config.mjs
@@ -4,6 +4,7 @@ import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default defineEcConfig({
   plugins: [pluginLineNumbers()],
+  themes: ["vitesse-dark", "vitesse-light"],
   shiki: {
     bundledLangs: ["sh", "bash", "typescript", "tsx"],
   },
