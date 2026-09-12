3f7a691f21114ca79fa331d4b2114d870386775b
aitorllamas <aitorllj93@gmail.com>
2026-09-12 14:49:57 +0200

    fix select spacing

--- registry/ui/pxl/select.tsx
+++ registry/ui/pxl/select.tsx
@@ -194,7 +194,7 @@ function SelectScrollUpButton({
     <SelectPrimitive.ScrollUpArrow
       data-slot="select-scroll-up-button"
       className={cn(
-        "top-0 z-10 flex w-full cursor-default items-center justify-center bg-popover py-1 [&_svg:not([class*='size-'])]:size-4",
+        "top-1.5 z-10 flex w-[calc(100%-(--spacing(3)))] cursor-default items-center justify-center bg-popover py-1 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
       {...props}
@@ -218,7 +218,7 @@ function SelectScrollDownButton({
     <SelectPrimitive.ScrollDownArrow
       data-slot="select-scroll-down-button"
       className={cn(
-        "bottom-0 z-10 flex w-full cursor-default items-center justify-center bg-popover py-1 [&_svg:not([class*='size-'])]:size-4",
+        "bottom-1.5 z-10 flex w-[calc(100%-(--spacing(3)))] cursor-default items-center justify-center bg-popover py-1 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
       {...props}
